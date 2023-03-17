da9147048fccc17c80534e964a41aabb77067f3c
mg6993136@gmail.com <mg6993136@gmail.com>
2023-03-16 18:36:25 -0700

    first

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-printf("Size of a char: %1d byte(s)\n", sizeof(char));
-printf("Size of an int: %4d byte(s)\n", sizeof(int));
+	printf("Size of a char: %1d byte(s)\n", sizeof(char));
+	printf("Size of an int: %4d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %4d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %8d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %4d byte(s)\n", sizeof(float));
