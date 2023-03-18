86c446d7994963a0d692bbb4452ed58c199914fe
mg6993136@gmail.com <mg6993136@gmail.com>
2023-03-17 19:23:55 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 252;
+
+	for (i = 0 ; i <= 15 ; i++)
+	{
+	printf("%x", i);
+	}
+	printf("\n");
+	return (0);
+}
