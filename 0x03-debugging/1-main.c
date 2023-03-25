165aba432c2afe20047c212fe70f52c795a0096a
mg6993136@gmail.com <mg6993136@gmail.com>
2023-03-24 17:26:53 -0700

    do

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,39 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 
-void positive_or_negative(int i);
-
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c);
-
-/**
- * print_remaining_days - takes a date and prints how many days are
- * * left in the year, taking leap years into account
- * * @month: month in number format
- * * @day: day of month
- * * @year: year
- * * Return: void
- * */
-
-void print_remaining_days(int month, int day, int year);
-
-/**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
+* main - causes an infinite loop
+* Return: 0
 */
 
-int convert_day(int month, int day);
+int main(void)
+{
+	int i;
 
-#endif /* MAIN_H */
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	*while (i < 10)
+	*{
+	*putchar(i);
+	*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
