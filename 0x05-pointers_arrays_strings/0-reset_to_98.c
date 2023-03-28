bd2e863f9ef56dfd6444e2d190bb6e7b87fe711c
mg6993136@gmail.com <mg6993136@gmail.com>
2023-03-27 19:38:31 -0700

    the power of ramadan

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
-
-void reset_to_98(int *);
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * reset_to_98 - takes a pointer as a parameter and change its value
+ * @n: parameter to change
+ * Return: 0 (Success)
  */
-int main(void)
-{
-	int n;
 
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
